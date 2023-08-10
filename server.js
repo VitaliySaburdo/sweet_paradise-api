09b62b529f1e08799851ef5422b7c1964b727417
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-10 21:25:46 +0300

    add files

---
+++ server.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3030;
+
+const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .then(app.listen(PORT))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
