3de75c04d5baf9cea96dab1d3d7a2fcb4d017d42
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-12 19:56:50 +0300

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,10 +7,11 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (user) {
-    throw HttpError(409, "Email in use");
-  }
+  console.log(req.body);
+  // const user = await User.findOne({ email });
+  // if (user) {
+  //   throw HttpError(409, "Email in use");
+  // }
 
   // const hashPassword = await bcrypt.hash(password, 10);
 
