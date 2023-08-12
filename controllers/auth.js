7dfb070ae3d4c1d5b01c7e11c690a7a11ba189c9
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-12 21:00:33 +0300

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,15 @@
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
 const { User } = require("../models/users");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
-// const { SECRET_KEY } = process.env;
-
 const register = async (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body);
-  // const user = await User.findOne({ email });
-  // if (user) {
-  //   throw HttpError(409, "Email in use");
-  // }
-
-  // const hashPassword = await bcrypt.hash(password, 10);
 
   const newUser = await User.create({
     ...req.body,
-    // password: hashPassword,
   });
-
-  // const payload = {
-  //   id: newUser._id,
-  // };
-
-  // const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-  // await User.findByIdAndUpdate(newUser._id, { token });
 
   res.status(201).json({
     message: "Account is successfully created",
-    // token: token,
     user: {
       name: newUser.name,
       email: newUser.email,
