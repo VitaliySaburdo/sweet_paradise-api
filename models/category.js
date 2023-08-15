eda478a363b657efad5ca359bd704f23cff1a75e
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-15 15:04:20 +0300

    fix

--- models/category.js
+++ models/category.js
@@ -2,19 +2,8 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const { handleMongooseError } = require("../helpers");
 
-let count = 1;
-
 const categorySchema = new Schema(
   {
-    _id: {
-      type: Number,
-      default: () => {
-        count++;
-        return count;
-      },
-      unique: true,
-      required: true,
-    },
     name: { type: String, required: true },
   },
   { versionKey: false }
