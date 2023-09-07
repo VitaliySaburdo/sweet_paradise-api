eee46b8324be8609c14906edaa97edb1ec40e7a2
VitaliySaburdo <vsaburdo@gmail.com>
2023-09-07 08:47:52 +0300

    fix

--- models/product.js
+++ models/product.js
@@ -40,10 +40,6 @@ const productSchema = new Schema(
       type: Boolean,
       default: false,
     },
-    quantity: {
-      type: Number,
-      default: 0,
-    },
   },
   { versionKey: false }
 );
